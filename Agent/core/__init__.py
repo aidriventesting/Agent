c5d4ce3ccaa496423ba0e7e86edde19ee37d8703
hassineabd <contact.abdelkaderhassine@gmail.com>
2025-12-22 23:49:47 +0100

    core update

--- Agent/core/__init__.py
+++ Agent/core/__init__.py
@@ -1,25 +1,7 @@
-from Agent.core.interfaces import (
-    PlatformProtocol,
-    CollectorProtocol,
-    LocatorProtocol,
-    ExecutorProtocol,
-)
-from Agent.core.models import (
-    BoundingBox,
-    WebElement,
-    AndroidElement,
-    IOSElement,
-)
+from Agent.core.interfaces import ExecutorProtocol
 from Agent.core.keyword_runner import KeywordRunner
 
 __all__ = [
-    "PlatformProtocol",
-    "CollectorProtocol",
-    "LocatorProtocol",
     "ExecutorProtocol",
     "KeywordRunner",
-    "BoundingBox",
-    "WebElement",
-    "AndroidElement",
-    "IOSElement",
 ]
