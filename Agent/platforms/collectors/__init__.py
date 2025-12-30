3528ae1953e17bf113e6b318dcda8d85337e6ff4
hassineabd <contact.abdelkaderhassine@gmail.com>
2025-12-30 12:19:17 +0100

    fixing init

--- Agent/platforms/collectors/__init__.py
+++ Agent/platforms/collectors/__init__.py
@@ -7,11 +7,11 @@ UI Collectors for mobile automation.
 
 from Agent.platforms.collectors.android_collector import AndroidCollector
 from Agent.platforms.collectors.ios_collector import IOSCollector
-from Agent.platforms.collectors.som_renderer import render_som, bbox_center
+from Agent.platforms.grounding.som.annotator import annotate_screenshot, bbox_center
 
 __all__ = [
     'AndroidCollector',
     'IOSCollector',
-    'render_som',
+    'annotate_screenshot',
     'bbox_center',
 ]
